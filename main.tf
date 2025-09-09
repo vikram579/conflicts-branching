6d15d2803de20a62a8cecd9efbcca3732b146cbf
sushma <sushmamca5565@gmail.com>
2025-09-09 14:54:15 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_db_instance" "primary" {
   identifier              = "my-primary-db"
   engine                  = "mysql"
-  engine_version          = "8.0.42"
+  engine_version          = "06-08-1992"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp2"
